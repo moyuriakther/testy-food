ece2727c992cfed4e2074b7c69a9885b159c9139
Moyuri Akther <akthermoyuri@.com>
2021-04-02 21:25:33 +0600

    up

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,19 +4,19 @@ import { useHistory } from "react-router";
 
 const Item = ({item}) => {
   const history = useHistory()
-  const handleOrder = (productName) => {
-      history.push(`/checkout/${productName}`);
+  const handleOrder = (name) => {
+      history.push(`/checkout/${name}`);
   }
 
   return (
     <Col md={4}>
-      <Card style={{ width: "18rem" }}>
+      <Card style={{ width: "18rem", marginBottom:"5px" }}>
         <Card.Img variant="top" src={item.imageURL} style={{height:'240px', width:'280px', padding: '30px'}} />
         <Card.Body>
-          <Card.Title>{item.productName}</Card.Title>
+          <Card.Title>{item.name}</Card.Title>
           <div style={{display:'flex', justifyContent:'space-between'}}>
                  <h5>${item.price}</h5>   
-                <Button onClick={() => handleOrder(item.productName)} variant="secondary">Buy Now</Button>
+                <Button onClick={() => handleOrder(item.name)} variant="secondary">Buy Now</Button>
           </div>
         </Card.Body>
       </Card>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,51 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
-import firebaseConfig from './firebase.config';
-import { UserContext } from '../../App';
-import { useHistory, useLocation } from 'react-router';
-
+import firebaseConfig from "./firebase.config";
+import { UserContext } from "../../App";
+import { useHistory, useLocation } from "react-router";
 
 if (firebase.apps.length === 0) {
-    firebase.initializeApp(firebaseConfig);
-  }
-
+  firebase.initializeApp(firebaseConfig);
+}
 
 const Login = () => {
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    const history = useHistory ();
-    const location = useLocation ();
-    const { from } = location.state || { from: { pathname: "/" } };
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const history = useHistory();
+  const location = useLocation();
+  const { from } = location.state || { from: { pathname: "/" } };
 
-    const handleSignIn = () =>{
-        var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithPopup(provider).then((result) => {
-    // var credential = result.credential;
-    // var token = credential.accessToken;
-    // var user = result.user;
-    // console.log(user);
-    const {displayName, email, photoURL} = result.user;
-    const signedInUser = {
-        issSignedIn: true,
-        name: displayName,
-        email: email,
-        photo: photoURL
-      };
-    setLoggedInUser(signedInUser);
-    history.replace(from);
-  }).catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    console.log(errorMessage, errorCode, email);
-  });
-    }
-    return (
-        <div>
-            <button onClick={handleSignIn}>Sign In With Google</button>
-        </div>
-    );
+  const handleSignIn = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        const signedInUser = {
+          issSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setLoggedInUser(signedInUser);
+        history.replace(from);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorMessage, errorCode, email);
+      });
+  };
+  return (
+    <div>
+      <button onClick={handleSignIn}>Sign In With Google</button>
+    </div>
+  );
 };
 
 export default Login;
